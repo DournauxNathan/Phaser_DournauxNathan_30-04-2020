d1ed5999fe93cb9c0dca06eb1cc634665fb623d9
DournauxNathan <dournauxnathan@gmail.com>
2020-04-13 10:55:35 +0200

    Déplacent
    Le personnage peut se dépalcer dans toutes les directions

--- test.js
+++ test.js
@@ -8,14 +8,53 @@ class test extends Phaser.Scene {
 	}
 
 	preload() {
-		
+		this.load.image('background','assetsProto/sky.png');
+		this.load.image('perso','assetsProto/purple.png');
 	}
 
 	create() {
 		
+		this.add.image(0,0,'background').setOrigin(0,0);
+
+	    this.physics.world.setBounds(0, 0, 1024*2.25, 768);
+
+	    this.player = this.physics.add.sprite(100,510,'perso');
+
+	    this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
+
+		this.keys = this.input.keyboard.addKeys('Z,Q,S,D');
 	}
 
 	update() {
-		    
+		//Déplacement
+			if(this.keys.Q.isDown)
+			{
+				this.player.setVelocityX(-200);
+				this.player.setFlipX(true);
+			}
+			else if(this.keys.D.isDown)
+			{
+				this.player.setVelocityX(200);
+				this.player.setFlipX(false);
+			}
+			else
+			{
+				this.player.setVelocityX(0);
+			}
+
+			if(this.keys.Z.isDown)
+			{
+				this.player.setVelocityY(-200);
+				this.player.setFlipX(true);
+			}
+			else if(this.keys.S.isDown)
+			{
+				this.player.setVelocityY(200);
+				this.player.setFlipX(false);
+			}
+			else
+			{
+				this.player.setVelocityY(0);
+			} 
 	}
 }
